969121f8da9cf1c232e274659fcd513a2fb87ab5
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-11-27 23:20:29 +0530

    added comments

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,16 @@
+// to load all env variables
 const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express')
 const app = express()
 
+// to use cors middleware to allow cross-origin requests
 const cors = require('cors')
+// allowing requests from everywhere
 app.use(cors())
 
+// route to test if server is running
 app.get('/',(req, res) => {
     res.send('Hello, Express!')
 })
